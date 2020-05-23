51250e9af2a5d1d097592e3e97e3b774acff1d5b
reactive_cat <mykola.papenko@gmail.com>
2020-05-23 19:03:04 +0300

    Ignore errors explicitly

--- main/main.go
+++ main/main.go
@@ -6,10 +6,10 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello, world!")
+	_, _ = io.WriteString(w, "Hello, world!")
 }
 
 func main() {
 	http.HandleFunc("/", hello)
-	http.ListenAndServe(":80", nil)
+	_ = http.ListenAndServe(":80", nil)
 }
